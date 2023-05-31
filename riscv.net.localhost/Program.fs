87f7b5e193f9a2beb1187e925ae54b5e97fcaa58
Muqiu Han <muqiu-han@outlook.com>
2023-05-31 12:51:40 +0800

    Add riscv.net.localhost tests

--- riscv.net.localhost/Program.fs
+++ riscv.net.localhost/Program.fs
@@ -1,2 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module riscv.net.localhost
+
+
+[<EntryPoint>]
+let main (args: string array) : int =
+    if args.Length < 1 then
+        printfn $"riscv.net.localhost binary"
+
+    let cpu = System.IO.File.ReadAllBytes(args[0]) |> riscv.net.core.CPU.CPU in
+
+    try
+        while cpu.PC < uint64 (cpu.DRAM.Length) do
+            cpu.Execute(cpu.Fetch())
+    with :? System.Exception as e ->
+        printfn $"{e.Message}"
+        cpu.DumpRegisters()
+
+    0
