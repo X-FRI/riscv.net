0bd63482d985262a7b454fda2e81039030478de4
Muqiu Han <muqiu-han@outlook.com>
2023-06-02 12:05:51 +0800

    Add some instruction implementation

--- riscv.net.core/CPU.fs
+++ riscv.net.core/CPU.fs
@@ -76,7 +76,7 @@ type CPU (code : array<uint8>) as self =
         match opcode with
         | 0x03UL ->
             let imm = uint64 ((inst |> int32 |> int64) >>> 20)
-            let addr = UInt64.wrapping_add(__regs[rs1], imm)
+            let addr = UInt64.wrapping_add (__regs[rs1], imm)
 
             match funct3 with
             // LB
@@ -108,13 +108,13 @@ type CPU (code : array<uint8>) as self =
             let shamt = (imm &&& 0x3FUL) |> uint32
 
             match funct3 with
-            | 0x0UL -> __regs[rd] <- UInt64.wrapping_add(__regs[rs1], imm)
+            | 0x0UL -> __regs[rd] <- UInt64.wrapping_add (__regs[rs1], imm)
 
             // SLLI
             | 0x1UL -> __regs[rd] <- __regs[rs1] <<< (shamt |> int32)
 
             // SLTI
-            | 0x2UL -> __regs[rd] <- if (__regs[rs1] |> int64) < (imm |> int64) then 1 else 0
+            | 0x2UL -> __regs[rd] <- if (__regs[rs1] |> int64) < (imm |> int64) then 1UL else 0UL
 
             // SLTIU
             | 0x3UL -> __regs[rd] <- if __regs[rs1] < imm then 1UL else 0UL
@@ -125,8 +125,13 @@ type CPU (code : array<uint8>) as self =
             // SRLI & SRAI
             | 0x5UL ->
                 match (funct7 >>> 1) with
-                | 0x00UL -> __regs[rd] <- UInt32.wrapping_shr(__regs[rs1] |> uint32, shamt |> int) |> int32 |> int64 |> uint64
-                | 0x10UL -> __regs[rd] <- Int32.wrapping_shr(__regs[rs1] |> int32, shamt |> int) |> int64 |> uint64
+                | 0x00UL ->
+                    __regs[rd] <-
+                        UInt32.wrapping_shr (__regs[rs1] |> uint32, shamt |> int)
+                        |> int32
+                        |> int64
+                        |> uint64
+                | 0x10UL -> __regs[rd] <- Int32.wrapping_shr (__regs[rs1] |> int32, shamt |> int) |> int64 |> uint64
                 | _ -> raise (IllegalInstruction(inst))
 
             // ORI
@@ -141,7 +146,7 @@ type CPU (code : array<uint8>) as self =
 
         // AUIPC
         | 0x17UL ->
-            __regs[rd] <- UInt64.wrapping_add(this.PC, ((inst &&& 0xFFFFF000UL) |> int32 |> int64 |> uint64))
+            __regs[rd] <- UInt64.wrapping_add (this.PC, ((inst &&& 0xFFFFF000UL) |> int32 |> int64 |> uint64))
             this.UpdatePC()
 
         | 0x1BUL ->
@@ -150,16 +155,17 @@ type CPU (code : array<uint8>) as self =
 
             match funct3 with
             // ADDIW
-            | 0x0UL -> __regs[rd] <- UInt64.wrapping_add(__regs[rs1], imm) |> int32 |> int64 |> uint64
+            | 0x0UL -> __regs[rd] <- UInt64.wrapping_add (__regs[rs1], imm) |> int32 |> int64 |> uint64
 
             // SLLIW
-            | 0x1UL -> __regs[rd] <- UInt64.wrapping_shr(__regs[rs1], shamt) |> int32 |> int64 |> uint64
+            | 0x1UL -> __regs[rd] <- UInt64.wrapping_shr (__regs[rs1], shamt) |> int32 |> int64 |> uint64
 
             // SRLIW & SRAIW
             | 0x5UL ->
                 match funct7 with
-                | 0x00UL -> __regs[rd] <- UInt32.wrapping_shr((__regs[rs1] |> uint32), shamt) |> int32 |> int64 |> uint64
-                | 0x20UL -> __regs[rd] <- Int32.wrapping_shr((__regs[rs1] |> int32),  shamt) |> int64 |> uint64
+                | 0x00UL ->
+                    __regs[rd] <- UInt32.wrapping_shr ((__regs[rs1] |> uint32), shamt) |> int32 |> int64 |> uint64
+                | 0x20UL -> __regs[rd] <- Int32.wrapping_shr ((__regs[rs1] |> int32), shamt) |> int64 |> uint64
                 | _ -> raise (IllegalInstruction(inst))
 
             | _ -> raise (IllegalInstruction(inst))
@@ -167,33 +173,139 @@ type CPU (code : array<uint8>) as self =
             this.UpdatePC()
 
         | 0x23UL ->
-            let imm = (((inst &&& 0xFE000000UL) |> int32 |> int64 >>> 20) |> uint64) ||| ((inst >>> 7) &&& 0x1F)
-            let addr = UInt64.wrapping_add(__regs[rs1], imm)
-            
+            let imm =
+                (((inst &&& 0xFE000000UL) |> int32 |> int64 >>> 20) |> uint64)
+                ||| ((inst >>> 7) &&& 0x1FUL)
+
+            let addr = UInt64.wrapping_add (__regs[rs1], imm)
+
             match funct3 with
             // SB
             | 0x0UL -> this.Store(addr, 8UL, __regs[rs2])
-            
+
             // SH
             | 0x1UL -> this.Store(addr, 16UL, __regs[rs2])
-            
+
             // SW
             | 0x2UL -> this.Store(addr, 32UL, __regs[rs2])
-            
+
             // SD
             | 0x3UL -> this.Store(addr, 64UL, __regs[rs2])
-            
-            | _ -> raise(UnreachableException())
-            
+
+            | _ -> raise (UnreachableException())
+
             this.UpdatePC()
 
         | 0x33UL ->
             let shamt = ((__regs[rs2] &&& 0x3FUL) |> uint64) |> uint32
 
             match (funct3, funct7) with
-            | (0x0UL, 0x00UL) -> __regs[rd] <- __regs[rs1] |+| __regs[rs2]
+            | (0x0UL, 0x00UL) -> __regs[rd] <- UInt64.wrapping_add (__regs[rs1], __regs[rs2])
             | _ -> raise (IllegalInstruction(inst))
 
             this.UpdatePC()
+
+        // LUI
+        | 0x37UL ->
+            __regs[rd] <- (inst &&& 0xFFFFF000UL) |> int32 |> int64 |> uint64
+            this.UpdatePC()
+
+        | 0x3BUL ->
+            let shamt = (__regs[rs2] &&& 0x1FUL) |> uint32
+
+            match (funct3, funct7) with
+
+            // ADDW
+            | (0x0UL, 0x00UL) ->
+                __regs[rd] <- UInt64.wrapping_add (__regs[rs1], __regs[rs2]) |> int32 |> int64 |> uint64
+
+            // SUBW
+            | (0x0UL, 0x20UL) -> __regs[rd] <- UInt64.wrapping_sub ((__regs[rs1], (__regs[rs2]))) |> int32 |> uint64
+
+            // SLLW
+            | (0x1UL, 0x00UL) ->
+                __regs[rd] <- UInt32.wrapping_shl ((__regs[rs1] |> uint32), shamt |> int) |> int32 |> uint64
+
+            // SRLW
+            | (0x5UL, 0x00UL) ->
+                __regs[rd] <- UInt32.wrapping_shr (__regs[rs1] |> uint32, shamt |> int) |> int32 |> uint64
+
+            // SRAW
+            | (0x5UL, 0x20UL) -> __regs[rd] <- ((__regs[rs1] |> int32) >>> (shamt |> int32)) |> uint64
+
+            | _ -> raise (IllegalInstruction(inst))
+
+            this.UpdatePC()
+
+        | 0x63UL ->
+            let imm =
+                (((inst &&& 0x80000000UL) |> int32 |> int64 >>> 19) |> uint64)
+                ||| ((inst &&& 0x80UL) <<< 4)
+                ||| ((inst >>> 20) &&& 0x7E0UL)
+                ||| ((inst >>> 7) &&& 0x1EUL)
+
+            match funct3 with
+
+            // BEQ
+            | 0x0UL ->
+                if __regs[rs1] = __regs[rs2] then
+                    UInt64.wrapping_add (this.PC, imm)
+                else
+                    this.UpdatePC()
+
+            // BNE
+            | 0x1UL ->
+                if __regs[rs1] <> __regs[rs2] then
+                    UInt64.wrapping_add (this.PC, imm)
+                else
+                    this.UpdatePC()
+
+            // BLT
+            | 0x4UL ->
+                if ((__regs[rs1] |> int64) < (__regs[rs2] |> int64)) then
+                    UInt64.wrapping_add (this.PC, imm)
+                else
+                    this.UpdatePC()
+
+            // BGE
+            | 0x5UL ->
+                if ((__regs[rs1] |> int64) >= (__regs[rs2] |> int64)) then
+                    UInt64.wrapping_add (this.PC, imm)
+                else
+                    this.UpdatePC()
+
+            // BLTU
+            | 0x6UL ->
+                if __regs[rs1] < __regs[rs2] then
+                    UInt64.wrapping_add (this.PC, imm)
+                else
+                    this.UpdatePC()
+
+            // BGEU
+            | 0x7UL ->
+                if __regs[rs1] >= __regs[rs2] then
+                    UInt64.wrapping_add (this.PC, imm)
+                else
+                    this.UpdatePC()
+
+            | _ -> raise (IllegalInstruction(inst))
+
+        // JALR
+        | 0x67UL ->
+            let imm = ((((inst &&& 0xFFF00000UL) |> int32) |> int64) >>> 20) |> uint64
+            __regs[rd] <- this.PC + 4UL
+            UInt64.wrapping_add (__regs[rs1], imm) &&& (~~~ 1UL)
+
+        // JAL
+        | 0x6fUL ->
+            __regs[rd] <- this.PC + 4UL
+
+            let imm =
+                (((inst &&& 0x80000000UL) |> int32 |> int64 >>> 11) |> uint64)
+                ||| (inst &&& 0xFF000UL)
+                ||| ((inst >>> 9) &&& 0x800UL)
+                ||| ((inst >>> 20) &&& 0x7FEUL)
+
+            UInt64.wrapping_add (this.PC, imm)
 
         | _ -> raise (IllegalInstruction(inst))
